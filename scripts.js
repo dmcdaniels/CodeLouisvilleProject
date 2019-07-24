fa7170577de64496bb1b3a15d982313777f16121
Dustin McDaniels <dmcdan06@gmail.com>
2019-07-24 19:53:26 -0400

    added JavaScript to change onclick

---
+++ scripts.js
@@ -0,0 +1,50 @@
+function change() {
+    var image = document.getElementById('BlackTest');
+    image.src = "images/PinkTestPic.png"
+
+    var text = document.getElementById('WordTest1');
+    text.innerHTML = "Bear enjoyed his pancakes very much. He would like to play with his friend Dog today. What should they do?";
+
+    var button1 = document.getElementById('button1');
+    button1.innerHTML = "Bear and Dog should play with blocks."
+
+    var button2 = document.getElementById('button2');
+    button2.innerHTML = "Bear and Dog should play with cars."
+    
+    var button1 = document.getElementById('button3');
+    button3.innerHTML = "Bear and Dog should color."
+}
+
+function change2() {
+    var image = document.getElementById('BlackTest');
+    image.src = "images/PinkTestPic.png"
+
+    var text = document.getElementById('WordTest1');
+    text.innerHTML = "Bear enjoyed his toast very much. He would like to play with his friend Dog today. What should they do?";
+
+    var button1 = document.getElementById('button1');
+    button1.innerHTML = "Bear and Dog should play with blocks."
+
+    var button2 = document.getElementById('button2');
+    button2.innerHTML = "Bear and Dog should play with cars."
+    
+    var button1 = document.getElementById('button3');
+    button3.innerHTML = "Bear and Dog should color."
+}
+
+function change3() {
+    var image = document.getElementById('BlackTest');
+    image.src = "images/PinkTestPic.png"
+
+    var text = document.getElementById('WordTest1');
+    text.innerHTML = "Bear enjoyed his favorite cereal very much. He would like to play with his friend Dog today. What should they do?";
+
+    var button1 = document.getElementById('button1');
+    button1.innerHTML = "Bear and Dog should play with blocks."
+
+    var button2 = document.getElementById('button2');
+    button2.innerHTML = "Bear and Dog should play with cars."
+    
+    var button1 = document.getElementById('button3');
+    button3.innerHTML = "Bear and Dog should color."
+}
